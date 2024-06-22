cda22c1c5bd2e3fda326c39ff8ea9825f71ee3d8
phamgiaphuc <acuscodinghcm@gmail.com>
2024-06-22 14:37:20 +0700

    Phuc | Add prisma and docker-compose.yml

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,3 +1,3 @@
-export default () => ({
+export default (): Record<string, unknown> => ({
   port: parseInt(process.env.PORT, 10) || 3000,
 });
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import { AppModule } from '@/app.module';
-import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from "@/app.module";
+import { Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: true,
   });
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('port');
+  const port = configService.get<number>("port");
   await app.listen(port);
   Logger.log(
-    `Application is running on: ${(await app.getUrl()).replace('[::1]', 'localhost')}`,
+    `Application is running on: ${(await app.getUrl()).replace("[::1]", "localhost")}`,
   );
 }
 bootstrap();
